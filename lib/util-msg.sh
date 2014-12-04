9c5403e6f37d3e48205d7ab87d4c1e21a9cf8433
udeved <flower_of_life@gmx.net>
2014-12-04 19:20:28 +0100

    add buildiso, mkiso,testiso

--- lib/util-msg.sh
+++ lib/util-msg.sh
@@ -8,6 +8,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+export LC_MESSAGES=C
 export LANG=C
 
 # check if messages are to be printed using color
